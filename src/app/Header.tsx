5ee317f4ffe59a6ba011335454efe958ef7f8e0e
Tan <lenhattan2313@gmail.com>
2024-10-19 09:07:49 +0700

    test

---
+++ src/app/Header.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Header() {
+  return (
+    <div>
+      Header <Button variant="destructive">CALL ACTION</Button>
+    </div>
+  );
+}
